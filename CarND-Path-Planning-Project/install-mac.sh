e459164130e9fd2d5108a3f3d531b3b898a7762a
Sebastian Rogawski <srogawsk@erikbeck-mobl1.ger.corp.intel.com>
2018-03-08 00:00:15 +0100

    fix for install-mac script

--- CarND-Path-Planning-Project/install-mac.sh
+++ CarND-Path-Planning-Project/install-mac.sh
@@ -3,13 +3,16 @@ brew install openssl libuv cmake zlib
 git clone https://github.com/uWebSockets/uWebSockets 
 cd uWebSockets
 git checkout e94b6e1
-patch CMakeLists.txt < ../cmakepatch.txt
+cd ..
+
+patch CMakeLists.txt < cmakepatch.txt
 mkdir build
 export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
 cd build
-cmake ..
+cmake -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2n -DOPENSSL_LIBRARIES=/usr/local/Cellar/openssl/1.0.2n/lib ..
+#cmake ..
 make 
 sudo make install
 cd ..
 cd ..
-sudo rm -r uWebSockets
+#sudo rm -r uWebSockets
